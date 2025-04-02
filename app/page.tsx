08da3328c532c3edef73ee3f20c55fb7eb185803
diptanshu1044 <diptanshubanerjee123@gmail.com>
2025-04-03 02:00:50 +0530

    feat: Add Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "@/components/HeroSection";
+import { ModeToggle } from "@/components/ModeToggle";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
